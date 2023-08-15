c8c4774daf3052fe5c82ad31f26b7a63441d910a
WebsiteF <980hammadakram@gmail.com>
2023-08-15 21:03:03 +0500

    updated code

--- Arrays Complete/How to initialize the whole array the with single value.cpp
+++ Arrays Complete/How to initialize the whole array the with single value.cpp
@@ -6,9 +6,13 @@ int main(){
 
     int arr[100];
     
-    fill(is_array_v, 100, 1);
+    // for filling all spaces with 1 
+    fill_n(arr, 100, 1);
 
-
+    // displaying array 
+    for(int i=0; i<100; i++){
+        cout<<arr[i]<<" ";
+    }
 
     return 0;
 }
