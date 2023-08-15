fdf693f760b1c6133dea13dd85f1ebe363304f60
WebsiteF <980hammadakram@gmail.com>
2023-08-15 18:48:09 +0500

    first commit

---
+++ Arrays Complete/How to initialize the whole array the with single value.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm> //Required for std::fill
+using namespace std;
+
+int main(){
+
+    int arr[100];
+    
+    fill(is_array_v, 100, 1);
+
+
+
+    return 0;
+}
+
