2857400e062c2cbab754319bc3dcbfc37a0a9d9d
hammad <979hammadakram@gmail.com>
2023-08-18 08:20:40 +0500

    bubble sort more optimized algo which reduced time complexity of best case

--- sortingAlgos/bubbleSort.cpp
+++ sortingAlgos/bubbleSort.cpp
@@ -1,16 +1,33 @@
 #include<iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n){
+// void bubbleSort(int arr[], int n){
    
+//    for(int i = 1; i < n; i++){
+//      for(int j = 0; j < n-i; j++){
+//         if(arr[j] > arr[j+1]){
+//             int temp = arr[j];
+//               arr[j] = arr[j+1];
+//               arr[j+1] = temp;
+//         }
+//      }
+//    }
+// };
+
+void bubbleSort(int arr[], int n){
+   //more optimized code
    for(int i = 1; i < n; i++){
+    bool swapped = false;
      for(int j = 0; j < n-i; j++){
         if(arr[j] > arr[j+1]){
             int temp = arr[j];
               arr[j] = arr[j+1];
               arr[j+1] = temp;
+              swapped = true;
         }
      }
+     if(swapped == false)
+       break;
    }
 };
 
