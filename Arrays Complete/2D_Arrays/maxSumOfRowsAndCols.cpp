4df62aab7bb373e2b8172b6b1e2ae3096f2472e8
hammad <979hammadakram@gmail.com>
2023-08-20 11:44:23 +0500

    2d array new problem added of max rowsum and max col sum

--- Arrays Complete/2D_Arrays/maxSumOfRowsAndCols.cpp
+++ Arrays Complete/2D_Arrays/maxSumOfRowsAndCols.cpp
@@ -2,26 +2,30 @@
 using namespace std;
 
 void rowWise(int arr[][4], int n, int m){
-    cout<<"printing row wise sum  = ";
+    int max = -1;
     for(int i=0; i<n; i++){
         int sum = 0;
         for(int j=0; j<m; j++){
            sum += arr[i][j];
         }
-        cout<<sum<<" ";
+        if(sum > max){
+         max = sum;
+        }  
     }
-    cout<<endl;
+    cout<<"maximum sum in rows is = "<<max<<endl;
 }
 
 void colWise(int arr[][4], int n, int m){
-    cout<<"printing column wise sum = ";
+    int max = -1;
     for(int j=0; j<m; j++){
         int sum = 0;
         for(int i=0; i<n; i++){
            sum += arr[i][j];
         }
-        cout<<sum<<" ";
+        if(sum > max)
+          max = sum;
     }
+     cout<<"maximum sum is in cols = "<<max<<endl;
 }
 
 int main(){
