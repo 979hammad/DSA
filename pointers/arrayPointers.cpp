6551b8e3edf0bc145606288e973ab14b634b33f7
hammad <979hammadakram@gmail.com>
2023-08-22 07:31:32 +0500

    Adding array pointers concepts

---
+++ pointers/arrayPointers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int arr[10] = {1, 2, 3, 4};
+  cout<<"Address of first memory block (arr) = "<<arr<<endl;
+  cout<<"Address of first memory block (&arr[0]) = "<<&arr[0]<<endl;
+  cout<<"Element at first memory block (arr[0]) = "<<arr[0]<<endl;
+  
+  cout<<"Element at first memory block (*arr) = "<<*arr<<endl;
+  //cout<<"Addition of 1 at first memory block (*arr + 1) = "<<*arr + 1<<endl; 
+  cout<<"Element at 2nd memory block (*(arr+1)) = "<<*(arr + 1)<<endl; // first incrementing the address by 4 bytes then displaying its value 
+  cout<<"Element at 1st memory blcok (*(arr)+1) = "<<*(arr)+1<<endl; // first point to the first memory block then incrementing its value by one
+  cout<<"Element at 3rd memory block (arr[2]) = "<<arr[2]<<endl;
+  cout<<"Element at 3rd memory block (*(arr+2)) = "<<*(arr + 2)<<endl; // first incremeting the address by 2 integers (8 bytes) then pointing to that value 
+  
+    return 0;
+}
