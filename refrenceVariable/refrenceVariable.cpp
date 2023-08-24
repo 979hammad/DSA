2c0b4711b835ff62a263c92e5c122b930d3789fe
hammad <979hammadakram@gmail.com>
2023-08-24 22:28:47 +0500

    adding some refrence and pointer variables bad practice

--- refrenceVariable/refrenceVariable.cpp
+++ refrenceVariable/refrenceVariable.cpp
@@ -9,6 +9,24 @@ void updateWithRef(int &n){
     n++;
 }
 
+// bad practice returing refrence variable 
+// returning a refrence or pointer variable is a bad practice
+
+int &update(int n){
+    int a = n;
+    int &ans = a;
+    //return a; //warning refrence to local variable a returned;--bad practice
+    return ans;
+}
+
+int *update2(int n){
+    int b = n;
+    int* ptr = &n;
+    
+    // return b; // --> bad practice can check eror (invalid conversion from int to int*) 
+    return ptr;
+}
+
 int main(){
 
    // refrence variable means aik variable k 2 naam // address same he rhe ga 
@@ -22,6 +40,10 @@ int main(){
    updateWithRef(n);
    cout<<"After updateWithoutRef() = "<<n<<endl;
 
- 
+   cout<<endl<<endl;
+
+   // some error concepts
+   cout<<"update(n) = "<<update(n)<<endl;
+   cout<<"update2(n) = "<<update2(n)<<endl;
     return 0;
 }
